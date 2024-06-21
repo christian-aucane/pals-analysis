5d951720f110adfe38e8d81960e70481c1391ebb
leo-carrey <leo.carrey@laplateforme.io>
2024-06-21 10:37:53 +0200

    [ADD] function diplay graph

--- src/streamlit-app.py
+++ src/streamlit-app.py
@@ -8,18 +8,21 @@ class Analysis:
 
     def load_data(self):
         self.db = pd.read_csv("raw_data/Palworld_Data-Tower BOSS attribute comparison.csv").T
+        
+    def display_graph(self):
         st.line_chart(self.db)
 
     def xx(self):
         pass
 
     def run(self):
+        self.load_data()
         st.write(""" # My first app""")
         st.sidebar.title("bruh")
         select = st.sidebar.selectbox("bruh", self.arr)
         self.categories = st.sidebar.multiselect('Select Filter',self.arr)
         if select == 1:
-            self.load_data()
+            self.display_graph()
         elif select == 2:
             self.xx()
 
