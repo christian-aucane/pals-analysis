3c85553fe934e4f0e3931ae262b00cc64650b26d
leo-carrey <leo.carrey@laplateforme.io>
2024-06-27 15:07:29 +0200

    [WIP] get data from sql

--- src/streamlit-app.py
+++ src/streamlit-app.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import streamlit as st
 from pandas import DataFrame, read_csv
-
+from utils.db.database import Database
 
 def get_transposed_csv(csv_path: str | Path) -> DataFrame:
     return read_csv(csv_path).T
@@ -9,17 +9,25 @@ def get_transposed_csv(csv_path: str | Path) -> DataFrame:
 
 DATA_PATH = Path("raw_data/Palworld_Data-Tower BOSS attribute comparison.csv")
 DATA = get_transposed_csv(DATA_PATH)
+db = Database(user= "root",password= "leo",host= "localhost",database= "palworld_database")
 
+def get_data(table_name: str, columns_names: list[str]):
+    return db.get_df_from_select(table_name=table_name,columns_names=columns_names)
 
 def display_app(data: DataFrame) -> None:
     select_box = [1, 2]
-    filters = [1, 2]
-    st.write(""" # My first app""")
+    filters = [
+        'name', 'hp', 'melee_attack', 'remote_attack', 'defense', 'support',
+        'experience_ratio', 'slow_walking_speed', 'walking_speed',
+        'running_speed', 'riding_speed', 'handling_speed', 'ignore_the_bluntness',
+        'ignore_displacement', 'biological_grade', 'endurance', 'fecundity']
+    filter_graphique = []
     st.sidebar.title("Title")
     _ = st.sidebar.selectbox("Select Box", select_box)
     _ = st.sidebar.multiselect(
         'Select Filter', filters)
-    st.line_chart(data)
+    hp = get_data("tower-boss-attribute", ["hp"])
+    st.line_chart(hp)
 
 
 def main() -> None:
